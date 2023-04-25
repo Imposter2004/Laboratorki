5e65998dc0cf0ca468e18002d256168ac802bfca
Impostor <davidka.aksenov.2004@mail.ru>
2023-04-25 17:40:45 +0500

    3 Laba

---
+++ 3 Лаба/3 Лаба/3 Лаба.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "Time.h"
+
+// Lab 3. Operations overloading
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"rus");
+    Time a;
+    Time b(1, 45);
+
+    cin >> a;
+
+    Time sum = a + b;
+
+    cout << a << " + " << b << " = " << sum << endl;
+
+    cout << "a == b : " << (a == b);
+}
